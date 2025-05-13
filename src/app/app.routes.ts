6a232e57fa0718d14c947976d413392e58135e7b
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-13 14:02:22 +0500

    bugfix 6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'login', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     loadComponent: () =>
